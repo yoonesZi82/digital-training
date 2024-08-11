8deb830bd58e95fba27a15fff1e5f88588bcbfb1
yoones <yooneszaamni58@gmail.com>
2024-08-11 16:15:15 +0330

    add loading

--- components/layout/LayoutPage.tsx
+++ components/layout/LayoutPage.tsx
@@ -1,25 +1,42 @@
 "use client";
-import React from "react";
-import { Layout } from "antd";
+import React, { useEffect, useState } from "react";
+import { Layout, Spin } from "antd";
 import Navbar from "../navbar/Nav";
 import FooterPage from "../footer/Footer";
-import { usePathname } from "next/navigation";
+import ScrollToTup from "@/utils/ScrollToTup";
+import AOSInit from "@/utils/aos";
 const { Content } = Layout;
 
 function LayoutPage({ children }: { children: React.ReactNode }) {
-  const path = usePathname();
+  const [loading, setLoading] = useState<boolean>(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
+
   return (
     <>
-      <Layout>
-        <Navbar />
-        <Content className="desktop:px-[152px] laptop:px-[152px] bg-normalWhite px-[53px] mobile:px-[53px] tablet:px[75px] py-[130px]">
-          <div>{children}</div>
-        </Content>
-        <FooterPage />
-        <div className="flex justify-center items-center bg-normalWhite w-full text-center">
-          <p className="text-gray-500 text-lg"> ساخته شده با ❤️ توسط YS</p>
+      {loading ? (
+        <div className="flex justify-center items-center w-full h-[100svh]">
+          <div className="loader"></div>
         </div>
-      </Layout>
+      ) : (
+        <>
+          <AOSInit />
+          <Layout>
+            <Navbar />
+            <Content className="desktop:px-[152px] laptop:px-[152px] bg-normalWhite px-[53px] mobile:px-[53px] tablet:px[75px] py-[130px]">
+              <div>{children}</div>
+            </Content>
+            <FooterPage />
+            <div className="flex justify-center items-center bg-normalWhite p-3 w-full text-center">
+              <p className="text-gray-500 text-lg"> ساخته شده با ❤️ توسط YS</p>
+            </div>
+          </Layout>
+          <ScrollToTup />
+        </>
+      )}
     </>
   );
 }
