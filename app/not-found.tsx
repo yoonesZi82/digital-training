096d40bba8b60d288da37400ed095016aae14296
yoones <yooneszaamni58@gmail.com>
2024-08-11 16:11:16 +0330

    create not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -14,7 +14,7 @@ const page: React.FC = () => (
           type="primary"
           size="large"
           icon={<PiArrowArcLeftBold size={20} />}
-          className="flex flex-row-reverse border-2 border-normalBlue bg-transparent hover:bg-normalBlue text-normalBlue hover:text-normalWhite transition-colors duration-500"
+          className="flex flex-row-reverse border-2 border-normalBlue bg-transparent hover:bg-normalBlue shadow-[10px_10px_10px_#babecc,-10px_-10px_20px_#ffffff] text-normalBlue hover:text-normalWhite transition-colors duration-500"
         >
           صفحه اصلی
         </Button>
