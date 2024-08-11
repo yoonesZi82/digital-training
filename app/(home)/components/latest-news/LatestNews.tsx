d140e5d49e6db8ee4d05625878565183876a91c5
yoones <yooneszaamni58@gmail.com>
2024-08-11 16:13:10 +0330

    fix bug

--- app/(home)/components/latest-news/LatestNews.tsx
+++ app/(home)/components/latest-news/LatestNews.tsx
@@ -4,9 +4,9 @@ import DrawerList from "../drawer/DrawerList";
 
 function LatestNews() {
   return (
-    <div className="container">
+    <div data-aos="fade-up" className="container">
       <div className="flex flex-col justify-center items-center gap-4 bg-normalWhite shadow-[10px_10px_10px_#babecc,-10px_-10px_20px_#ffffff] my-12 p-12 rounded-xl w-full text-center">
-        <p className="font-medium text-3xl text-normalBlue">
+        <p className="font-medium text-3xl text-normalBlue leading-[45px]">
           برایه مشاهده اخرین اخبار کلیک کنید 👇
         </p>
         <DrawerList />
